dc7d7f7950745fd2fc76a84c8c823cbda829b083
Nick-McComb <128945764+Nick-McComb@users.noreply.github.com>
2024-04-30 16:58:01 -0400

    installed pyqt-tools

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-import numpy as np
-import math as math
+#import numpy as np
+#import math as math
+from PyQt6.QtWidgets import QWidget, QApplication
+import sys 
+from PyQt6.QtGui import QIcon
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Calculator")
+        self.setWindowIcon(QIcon("calculator.png"))
+        self.setFixedHeight(500)
+        self.setFixedWidth(350)
+        
+    
+
+app = QApplication([])
+window = Window()
+window.show()
+sys.exit(app.exec())
 
 
