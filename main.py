a27bfce48e2f7e5de9dffc9593f98d8691cadf63
Nick-McComb <128945764+Nick-McComb@users.noreply.github.com>
2024-05-11 22:40:15 -0400

    fixed minor changes

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ class Window(QWidget):
         self.mk_button("9",214,456)
         self.mk_button("(", 146,420,'#303332', 13, 5000, "white")
         self.mk_button(")",214,420, '#303332', 13, 5000, "white")
-        self.mk_button("^",282,384,'#303332', 15, 5000, "white","pi" )
+        self.mk_button("^",282,384,'#303332', 15, 5000, "white","π" )
         self.mk_button("2nd",10,276,'#788b9c', 10, 551, "white")
         
 
@@ -52,8 +52,8 @@ class Window(QWidget):
         self.mk_button("*",282,456, '#A9A7A7', 18)
         self.mk_button("/",282,420, '#A9A7A7',20)
 #CLEAR/ENTER
-        self.mk_button("ENTER",282,564,'#A9A7A7', 10, 551)
-        self.mk_button("CLEAR",282,348,'#303332', 10, 551, "white")
+        self.mk_button("ENTER",282,564,'#A9A7A7', 10, 551, "black", "ENTER")
+        self.mk_button("CLEAR",282,348,'#303332', 10, 551, "white", "CLEAR")
 #add screen
         self.create_widgets()
 
@@ -98,63 +98,59 @@ class Window(QWidget):
                 self.buttons[i].setText(self.sec_text[i])
 
 
-        #elif something == "ENTER":
+        elif something == "ENTER":
             #self.new_line_text= str(eval(self.hidden_text.split('\n')[self.n]))
-            #self.new_line_text= str(eval(self.hidden_text))
-            #self.label.setText(current_text + "\n" + self.new_line_text + "\n")
-            #self.hidden_text = ""
-        #elif something == "CLEAR":
-             #current_text = ""
-             #self.new_line_text = ""
-             #self.hidden_text = ""
-             #self.label.setText(current_text)
+            self.new_line_text= str(eval(self.hidden_text))
+            self.label.setText(current_text + "\n" + self.new_line_text + "\n")
+            self.hidden_text = ""
+        elif something == "CLEAR":
+             current_text = ""
+             self.new_line_text = ""
+             self.hidden_text = ""
+             self.label.setText(current_text)
         elif something != "2nd" and self.n==0:
-                if something == "ENTER":
-                #self.new_line_text= str(eval(self.hidden_text.split('\n')[self.n]))
-                        self.new_line_text= str(eval(self.hidden_text))
-                        self.label.setText(current_text + "\n" + self.new_line_text + "\n")
-                        self.hidden_text = ""
-                elif something == "CLEAR":
-                        current_text = ""
-                        self.new_line_text = ""
-                        self.hidden_text = ""
-                        self.label.setText(current_text)
-                elif something == "+" or something == "-" or something == "/" or something == "*":
+                if something == "+" or something == "-" or something == "/" or something == "*":
                     current_text = current_text + self.new_line_text 
                     self.hidden_text = self.hidden_text + self.new_line_text 
-                    self.label.setText(current_text+something)
-                    self.hidden_text = self.hidden_text + something
-                    self.new_line_text = ""
-                    print(self.hidden_text)
+                    #self.label.setText(current_text+something)
+                    #self.hidden_text = self.hidden_text + something
+                    #self.new_line_text = ""
+                    #print(self.hidden_text)
                         
-                elif len(current_text) > 0 and something == "(" and current_text[-1].isdigit() or len(current_text) > 0 and something == "(" and current_text[-1] == ")": 
+                elif len(current_text) > 0 and something == "(" and current_text[-1].isdigit() or len(current_text) > 0 and something == "(" and current_text[-1] == ")" or len(current_text) > 0 and something == "(" and current_text[-1] == "π": 
                      self.hidden_text = self.hidden_text + "*"
-                     self.label.setText(current_text+something)
-                     self.hidden_text = self.hidden_text + something
-                     self.new_line_text = ""
-                     print(self.hidden_text)
-                else:
-                        self.label.setText(current_text+something)
-                        self.hidden_text = self.hidden_text + something
-                        self.new_line_text = ""
-                        print(self.hidden_text)
+                     #self.label.setText(current_text+something)
+                     #self.hidden_text = self.hidden_text + something
+                     #self.new_line_text = ""
+                     #print(self.hidden_text)
+                elif len(current_text) > 0 and something.isdigit() and current_text[-1] == ")":
+                     self.hidden_text = self.hidden_text + "*"
+                
+                self.label.setText(current_text+something)
+                self.hidden_text = self.hidden_text + something
+                self.new_line_text = ""
+                print(self.hidden_text)
 
         elif something != "2nd" and self.n ==1:
-            print(second_something)
-            if len(current_text) > 0 and second_something == "pi" and current_text[-1].isdigit():
-                current_text = current_text + "π"
+            #print(second_something)
+            if len(current_text) > 0 and second_something == "π" and current_text[-1].isdigit() or len(current_text) > 0 and second_something == "π" and current_text[-1] == ")":
+                self.label.setText(current_text+second_something)
                 self.hidden_text = self.hidden_text + "*np.pi"
-            elif second_something == "pi":
-                current_text = current_text + "π"
+                self.new_line_text = ""
+                print(self.hidden_text)
+            elif second_something == "π":
+                self.label.setText(current_text+second_something)
                 self.hidden_text = self.hidden_text + "np.pi"
+                self.new_line_text = ""
+                print(self.hidden_text)
             else:
                 self.label.setText(current_text+second_something)
                 self.hidden_text = self.hidden_text + second_something
                 self.new_line_text = ""
-                print(self.hidden_text)
+                print("poop")
                 
 
-x = 4 * np.pi
+x = (4)*np.pi
 print(x)
 app = QApplication([])
 window = Window()
